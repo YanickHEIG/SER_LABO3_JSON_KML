a75483fdd2eadc0d2b9cebaaaca2e81422384f7a
YanickHEIG <yanick.thomann@heig-vd.ch>
2021-05-23 15:45:40 +0200

    KMLWriter class created

---
+++ JSON-KML/src/main/java/KMLWriter.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import org.jdom2.Document;
+import org.jdom2.Element;
+
+public class KMLWriter {
+
+    public KMLWriter(String outfilePath, ArrayList<Country> countries){
+        Document outfile = new Document(new Element("kml"));
+    }
+
+}
