b57e5b84d7cc6027956dd25ee5d6792d0457b822
YanickHEIG <yanick.thomann@heig-vd.ch>
2021-05-26 16:09:15 +0200

    Fini, ajouté rendu .zip

--- JSON-KML/src/main/java/Main.java
+++ JSON-KML/src/main/java/Main.java
@@ -1,19 +1,8 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
         GeoJsonParser geoJsonParser = new GeoJsonParser();
         geoJsonParser.parse("../countries.geojson");
-
 
         KMLWriter kmlWriter = new KMLWriter();
         kmlWriter.write("../countries.kml", geoJsonParser.getCountries());
